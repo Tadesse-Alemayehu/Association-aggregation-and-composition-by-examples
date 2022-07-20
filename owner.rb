cbbaa04a4552098d113d557c690fb2a676639de6
Tadesse-Alemayehu <tadalem26@gmail.com>
2022-07-20 15:32:19 +0300

    Update animal class

--- owner.rb
+++ owner.rb
@@ -1,13 +1,21 @@
-class owner
-  attr_accessor :mame
+require_relative 'animal'
+
+class Owner
+  attr_accessor :name
   attr_reader :animals
 
   def initialize(name)
-    @mame=name
-    @animals=[];
+    @name=name
+    @animals=[]
   end
 
   def add_animal(animal)
-    @aimals.push(animal)
+    @animals.push(animal)
+    animal.owner=self
   end
 end
+
+animal=Animal.new(4, 4, 'Rex')
+owner=Owner.new('tadesse alemayehu ')
+owner.add_animal(animal)
+puts owner.animals
